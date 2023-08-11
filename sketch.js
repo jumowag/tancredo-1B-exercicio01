91affbfae19b195239ddd45d1f73228ab7ae1021
Wagner Yedro LIma <106695902+jumowag@users.noreply.github.com>
2023-08-11 17:41:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(600, 600);
+  background("orange");
+}
+
+function draw() {
+  
+ fill("gold") 
+  stroke("yellow")
+ 
+  
+  if(mouseIsPressed){
+     rect(mouseX,mouseY,30, 50);
+  }
+  
+  
+  
+}
